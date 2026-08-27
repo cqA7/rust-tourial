56a9d3ed8ae06866b144d84e72738a1a03c8a4fd
cqA7 <23746485@qq.com>
2026-08-27 17:57:39 +0800

    chore: reTest base-type

--- base-type/src/demo.rs
+++ base-type/src/demo.rs
@@ -1,7 +1,26 @@
 pub fn integer() {
     let i_num: u32 = 100_100;
     let u_num: i32 = -100_100;
-    println!("i_num is {},\nu_num is {}", i_num, u_num)
+    println!("i_num is {},\nu_num is {}", i_num, u_num);
+
+    let a: u8 = 255;
+    let added: u8 = 20;
+    let b = a.saturating_add(added);
+    println!("{} saturating_add {} b is {}", a, added, b);
+    let c = a.wrapping_add(added);
+    println!("{} wrapping_add {} b is {}", a, added, c);
+}
+
+pub fn float() {
+    let (a, b, c): (f32, f64, f64) = (0.1, 0.2, 0.3);
+    println!("a, b, c is {}, {}, {}", a, b, c);
+}
+
+pub fn nan() {
+    let x = (-42.0_f32).sqrt();
+    if x.is_nan() {
+        println!("未定义的数学行为");
+    }
 }
 
 pub fn greet_world() {
@@ -41,42 +60,4 @@ pub fn greet_world() {
             println!("{}, {}cm", name, length);
         }
     }
-}
-
-pub fn variable() {
-    let (a, mut b): (bool, bool) = (true, false);
-    println!("a = {}, b = {}", a, b);
-    b = true;
-    println!("after change a = {}, b = {}", a, b);
-    assert_eq!(a, b);
-    const MAX_COUNT: u32 = 1000;
-    println!("MAX_COUNT: {}", MAX_COUNT);
-    // 无符号8位整数，二进制为00000010
-    let a: u8 = 2; // 也可以写 let a: u8 = 0b_0000_0010;
-
-    // {:08b}：左高右低输出二进制01，不足8位则高位补0
-    println!("a value is        {:08b}", a);
-}
-
-pub fn char_bool() {
-    let x = '中';
-    println!("字符'中'占用了{}字节的内存大小", size_of_val(&x));
-}
-
-// 函数就是一个表达式
-// Rust 的函数体是由一系列语句组成，最后由一个表达式来返回值，例如：
-// 表达式会进行求值，语句不会
-pub fn statement_expression(x: i32) -> i32 {
-    let x = x + 1;
-    let y = if x % 2 == 1 { "odd" } else { "even" };
-    println!("y is {}", y);
-    x
-}
-
-pub fn add(x: i32, y: i32) -> i32 {
-    x + y // 没有分号，表示这是一个表达式，返回值是 x + y 的结果
-}
-
-pub fn plus_or_minus(x: i32) -> i32 {
-    if x > 5 { x - 5 } else { x + 5 }
 }
--- base-type/src/main.rs
+++ base-type/src/main.rs
@@ -1,16 +1,10 @@
 mod demo;
 
-use demo::{add, char_bool, greet_world, integer, plus_or_minus, statement_expression, variable};
+use demo::{float, greet_world, integer, nan};
 
 fn main() {
     integer();
+    float();
+    nan();
     greet_world();
-    variable();
-    char_bool();
-    let y = statement_expression(10);
-    println!("y = {}", y);
-    let sum = add(10, 4);
-    println!("sum is {}", sum);
-    let res = plus_or_minus(10);
-    println!("plus_or_minus(10) = {}", res);
 }
